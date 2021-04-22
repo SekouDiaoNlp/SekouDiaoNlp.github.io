02e30074567f8209cb68e97675f29d121979888c
SekouDiaoNlp <diao.sekou.nlp@gmail.com>
2021-04-22 16:31:10 +0200

    Enabled the google analytics feature.

--- website/publishconf.py
+++ website/publishconf.py
@@ -20,5 +20,5 @@ DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
-#DISQUS_SITENAME = ""
-GOOGLE_ANALYTICS = "G-T305ZX5D6R"
+# DISQUS_SITENAME = ""
+GOOGLE_ANALYTICS = u"G-T305ZX5D6R"
